3a914a23d45f227e4ccc388ff34c3540da03b0b1
katie <kjewett17@gmail.com>
2023-03-26 14:10:03 -0400

    styling
    adding css styles and updating octocat image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {useEffect} from 'react';
 import './styles/globals.css';
+import './styles/header.css';
+import './styles/search.css';
+import './styles/profile.css';
 
 
 function App() {
@@ -36,36 +39,39 @@ function App() {
   
   return (
 
-      <article className="App">
-        <header>
+      <main className="App">
+        <div className="main-container">
+        <header className='header'>
           <h1>devfinder</h1>
-          <h2>Light </h2>
+          {/* <h2>Light </h2> */}
+          <div className="theme-container">
+          <div className='theme-name'> Dark</div>
           <div className="theme-switch-wrapper">
             <label className="theme-switch" type="checkbox">
                 <input type="checkbox" id="checkbox" />
+                <label for="checkbox"><img src="icon-moon.svg" /></label>
                 <div className="slider round"></div>
           </label>
+          </div>
         </div>
-          <h2> Dark</h2>
-          <img src="/icon-moon.svg" alt="moon" className='img-sun'/>
+          
+          {/* <img src="/icon-moon.svg" alt="moon" className='theme-img'/> */}
         </header>
 
         <section className="search-bar">
-          <img src="icon-search.svg" alt="search" />
+          <img src="icon-search.svg" className="search-icon" alt="search" />
           <input type="text" placeholder="Search GitHub username..."/>
           <input type="submit" value="Search" />
         </section>
 
         <section className="profile">
           <div className="profile-banner">
-            <div className="avatar">
-              <img src="octocat.png" alt="avatar" />
-            </div>
+            <img src="the-octocat.png" className= 'avatar-image' alt="avatar" />
 
             <div className="profile-details">
-              <h1>The Octocat</h1>
-              <h3>@octocat</h3>
-              <h3>Joined 25 Jan 2011</h3>
+              <h2>The Octocat</h2>
+              <h3 className='tag'>@octocat</h3>
+              <div>Joined 25 Jan 2011</div>
             </div>
           </div>
 
@@ -74,46 +80,47 @@ function App() {
           </div>
 
           <div className="profile-stats">
-            <div className="repos">
-              <h4 className="repos-title">Repos</h4>
-              <h4 className="repos-value">8</h4>
+            <div className="metric">
+              <h4 className="metric-title">Repos</h4>
+              <h4 className="metric-value">8</h4>
             </div>
 
-            <div className="followers">
-              <h4 className="followers-title">Followers</h4>
-              <h4 className="followers-value">3938</h4>
+            <div className="metric">
+              <h4 className="metric-title">Followers</h4>
+              <h4 className="metric-value">3938</h4>
             </div>
 
-            <div className="following">
-              <h4 className="following-title">Following</h4>
-              <h4 className="following-value">9</h4>
+            <div className="metric">
+              <h4 className="metric-title">Following</h4>
+              <h4 className="metric-value">9</h4>
             </div>
           </div>
 
           <div className="profile-socials">
-            <div className="location">
-              <img src="icon-location.svg" alt="map icon" />
+            <div className="social">
+              <img src="icon-location.svg" className="social-img" alt="map icon" />
               <h4>San Francisco</h4>
             </div>
 
-            <div className="website">
-              <img src="icon-website.svg" alt="website" />
+            <div className="social">
+              <img src="icon-website.svg" className="social-img" alt="website" />
               <h4>https://github.blog</h4>
             </div>
 
-            <div className="twitter">
-              <img src="icon-twitter.svg" alt="twitter logo" />
+            <div className="social">
+              <img src="icon-twitter.svg" className="social-img" alt="twitter logo" />
               <h4>Not Available</h4>
             </div>
 
-            <div className="company">
-              <img src="icon-company.svg" alt="company icon" />
+            <div className="social">
+              <img src="icon-company.svg" className="social-img" alt="company icon" />
               <h4>@github</h4>
             </div>
           </div>
         </section>
+        </div>
           
-      </article>
+      </main>
    
   
   );
